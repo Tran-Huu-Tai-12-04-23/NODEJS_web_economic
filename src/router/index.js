9fe734f232d95347a5098788fbf675f8280f497e
huutai <huutaitran201@gmail.com>
2022-09-19 18:18:51 +0700

    aad new askdasld

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,8 @@ const newsRouter = require('./news');
 function router(app) {
         app.use('/home', homeRouter);
     app.
-    use('/news', newsRouter);
+    use('/news',
+     newsRouter);
 }
 
 module.exports = router;
